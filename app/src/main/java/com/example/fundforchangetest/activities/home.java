75801121fb483abbd8c3d78c57088f80de557b9c
Abdulla Al Mued <abdullaalmuid101@gamil.com>
2022-02-19 22:04:37 +0600

    search event complete

--- app/src/main/java/com/example/fundforchangetest/activities/home.java
+++ app/src/main/java/com/example/fundforchangetest/activities/home.java
@@ -1,13 +1,21 @@
 package com.example.fundforchangetest.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
+import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.EditText;
 
 import com.example.fundforchangetest.Models.Model;
 import com.example.fundforchangetest.R;
@@ -28,20 +36,43 @@ public class home extends AppCompatActivity {
 
 
     RecyclerView recview;
-    List<Model> datalist;
+    List<Model> datalist,backup;
     FirebaseFirestore db;
     commonRecyclerAdapter adapter;
     private CircleImageView profileBtn;
     //private static int SPLASH_TIME= 1;
+    private static final String TAG = "FirestoreSearchActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
+        EditText searchBox = findViewById(R.id.search_box);
+        searchBox.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+                //Log.d(TAG, "SearchBox has change to : "+ s.toString());
+                processSearch(s.toString().toLowerCase().trim());
+
+            }
+        });
+
         recview = findViewById(R.id.rec_view);
         recview.setLayoutManager(new LinearLayoutManager(this));
         datalist = new ArrayList<>();
+        backup = new ArrayList<>();
 
         adapter = new commonRecyclerAdapter(this,datalist);
         recview.setAdapter(adapter);
@@ -127,4 +158,41 @@ public class home extends AppCompatActivity {
                 });
 
     }
+
+
+
+    // new added
+
+    private void processSearch(String query) {
+
+        db.collection("event").whereEqualTo("status","accepted")
+                .get()
+                .addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+                    @Override
+                    public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
+                        List<DocumentSnapshot> list = queryDocumentSnapshots.getDocuments();
+
+
+                        backup.clear();
+                        for(DocumentSnapshot d : list){
+                            String data = d.getString("name").toLowerCase();
+                            if(data.contains(query)) {
+                                Model obj = d.toObject(Model.class);
+                                //obj.setId(d.getId());
+                                backup.add(obj);
+                            }
+
+                        }
+                        //adapter.notifyDataSetChanged();
+                        adapter = new commonRecyclerAdapter(home.this,backup);
+                        recview.setAdapter(adapter);
+
+
+
+                    }
+                });
+
+    }
+
+
 }
